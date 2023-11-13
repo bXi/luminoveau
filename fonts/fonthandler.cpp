47cca971b437c95a589241d639c08f19bd024f63
Dennis Meinen <bluepunk@gmail.com>
2023-11-13 23:20:14 +0100

    Added SDL3_ttf to Cmake and updated Font system to load/render fonts using this.

--- fonts/fonthandler.cpp
+++ fonts/fonthandler.cpp
@@ -9,7 +9,13 @@ Font Fonts::_getFont(const char* fileName, const int fontSize)
 	if (_fonts.find(index) == _fonts.end()) {
 
 		Font _font;
-        //TODO: load font here using SDL_ttf
+        _font.font = TTF_OpenFont(fileName, fontSize);
+
+        if (_font.font == nullptr) {
+            SDL_Log("Couldn't load %d pt font from %s: %s\n",
+                    fontSize, fileName, SDL_GetError());
+        }
+
 		_fonts[index] = _font;
 
 		return _fonts[index];
@@ -17,6 +23,24 @@ Font Fonts::_getFont(const char* fileName, const int fontSize)
 	else {
 		return _fonts[index];
 	}
+}
+
+void Fonts::_drawText(const char *fileName, const int fontSize, vf2d pos, const char *textToDraw, Color color) {
+
+    SDL_Surface *textSurface;
+
+    Font font = _getFont(fileName, fontSize);
+
+    SDL_Color c = {(Uint8)color.r, (Uint8)color.g, (Uint8)color.b, (Uint8)color.a};
+    textSurface = TTF_RenderUTF8_Blended(font.font, textToDraw, c);
+
+    SDL_FRect dstRect = {pos.x, pos.y, (float)textSurface->w, (float)textSurface->h};
+    SDL_FRect srcRect = {0.f, 0.f, (float)textSurface->w, (float)textSurface->h};
+
+    auto tex = SDL_CreateTextureFromSurface(Window::GetRenderer(), textSurface);
+    SDL_RenderTexture(Window::GetRenderer(), tex, &srcRect, &dstRect);
+
+    SDL_DestroyTexture(tex);
 }
 
 //*/
--- fonts/fonthandler.h
+++ fonts/fonthandler.h
@@ -3,31 +3,45 @@
 #include <unordered_map>
 //*
 #include "configuration/configuration.h"
+#include "window/windowhandler.h"
+
 #include "utils/helpers.h"
+#include "utils/colors.h"
+
+#include "SDL3_ttf/SDL_ttf.h"
+
 struct Font
 {
-
+    TTF_Font* font = nullptr;
 };
 
 
 class Fonts {
 public:
-    static Font getFont(const char* fileName, const int fontSize)
+    static Font GetFont(const char* fileName, const int fontSize)
     {
         return get()._getFont(fileName, fontSize);
+    }
+
+    static void DrawText(const char* fileName, const int fontSize, vf2d pos, const char* textToDraw, Color color)
+    {
+        get()._drawText(fileName, fontSize, pos, textToDraw, color);
     }
 
 private:
     std::unordered_map<const char*, Font> _fonts;
 
-    Font _getFont(const char* fileName, const int fontSize);
+    Font _getFont(const char* fileName, int fontSize);
+    void _drawText(const char* fileName, const int fontSize, vf2d pos, const char* textToDraw, Color color);
 
 public:
     Fonts(const Fonts&) = delete;
     static Fonts& get() { static Fonts instance; return instance; }
 private:
-    Fonts() {}
+    Fonts() {
+        TTF_Init();
+    }
     ;
 };
 
-//*/
+//*/;
