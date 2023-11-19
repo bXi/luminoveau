50093f167ac5f5318f888fb56fd1d6f17ac06d7f
Dennis Meinen <bluepunk@gmail.com>
2023-11-19 16:53:10 +0100

    Implemented a bunch of missing methods in Render2D

--- render2d/render2dhandler.cpp
+++ render2d/render2dhandler.cpp
@@ -7,7 +7,7 @@ void Render2D::_drawRectangle(vf2d pos, vf2d size, Color color) {
     SDL_RenderRect(renderer, &rect);
 }
 
-void Render2D::_drawTexture(Texture texture, vf2d pos, vf2d size) {
+void Render2D::_drawTexture(Texture texture, vf2d pos, vf2d size, Color color) {
 
     SDL_FRect dstRect = {pos.x, pos.y, size.x, size.y};
     SDL_FRect srcRect = {0.f, 0.f, (float)texture.width, (float)texture.height};
@@ -19,13 +19,80 @@ void Render2D::_drawTexture(Texture texture, vf2d pos, vf2d size) {
     SDL_DestroyTexture(tex);
 }
 
-void Render2D::_drawTexturePart(Texture texture, vf2d pos, vf2d size, Rectangle src) {
+void Render2D::_drawTexturePart(Texture texture, vf2d pos, vf2d size, Rectangle src, Color color) {
     SDL_FRect dstRect = {pos.x, pos.y, size.x, size.y};
     SDL_FRect srcRect = src;
 
     auto tex = SDL_CreateTextureFromSurface(renderer, texture.surface);
 
+
+    if (color.r < 255 || color.g < 255 || color.b < 255) {
+        SDL_SetTextureColorMod(tex, color.r, color.g, color.b < 255);
+    }
+
+    if (color.a < 255) {
+        SDL_SetTextureAlphaMod(tex, color.a);
+    }
+
+
     SDL_RenderTexture(renderer, tex, &srcRect, &dstRect);
 
     SDL_DestroyTexture(tex);
+}
+
+void Render2D::_drawCircle(vf2d pos, float radius, Color color) {
+
+    //TODO verify
+    SDL_SetRenderDrawColor(renderer, color.r, color.g,color.b,color.a);
+
+    int x = radius;
+    int y = 0;
+    int err = 0;
+
+    while (x >= y) {
+
+        // Draw points in all eight octants
+        SDL_RenderPoint(renderer, pos.x + x, pos.y - y);
+        SDL_RenderPoint(renderer, pos.x + y, pos.y - x);
+        SDL_RenderPoint(renderer, pos.x - y, pos.y - x);
+        SDL_RenderPoint(renderer, pos.x - x, pos.y - y);
+        SDL_RenderPoint(renderer, pos.x - x, pos.y + y);
+        SDL_RenderPoint(renderer, pos.x - y, pos.y + x);
+        SDL_RenderPoint(renderer, pos.x + y, pos.y + x);
+        SDL_RenderPoint(renderer, pos.x + x, pos.y + y);
+
+        if (err <= 0) {
+            y += 1;
+            err += 2 * y + 1;
+        }
+        if (err > 0) {
+            x -= 1;
+            err -= 2 * x + 1;
+        }
+    }
+
+}
+
+void Render2D::_drawRectangleRounded(vf2d pos, vf2d size, float radius, Color color) {
+    //TODO: implement this
+
+    _drawRectangle(pos,size,color);
+}
+
+void Render2D::_drawLine(vf2d start, vf2d end, float width, Color color) {
+    SDL_SetRenderDrawColor(renderer, color.r, color.g,color.b,color.a);
+    SDL_RenderLine(renderer, start.x, start.y, end.x, end.y);
+}
+
+void Render2D::_drawCircleSector(vf2d center, float radius, float startAngle, float endAngle, int segments, Color color) {
+
+}
+
+void Render2D::_beginScissorMode(Rectangle area) {
+    const SDL_Rect cliprect = area;
+    SDL_SetRenderClipRect(renderer, &cliprect);
+}
+
+void Render2D::_endScissorMode() {
+    SDL_SetRenderClipRect(renderer, nullptr);
 }
--- render2d/render2dhandler.h
+++ render2d/render2dhandler.h
@@ -5,6 +5,7 @@
 #include <optional>
 
 #include "SDL3/SDL.h"
+#include "SDL3/SDL_shape.h"
 
 #include "texture/texturehandler.h"
 #include "window/windowhandler.h"
@@ -14,30 +15,45 @@
 
 class Render2D {
 public:
-    static void DrawRectangle(vf2d pos, vf2d size, Color color)
-    {
-        get()._drawRectangle(pos, size, color);
-    };
+    [[maybe_unused]] static void DrawLine(vf2d start, vf2d end, float width, Color color) { get()._drawLine(start, end, width, color); };
 
-    static void DrawTexture(Texture texture, vf2d pos, vf2d size)
-    {
-        get()._drawTexture(texture, pos, size);
-    };
+    [[maybe_unused]] static void DrawRectangle(vf2d pos, vf2d size, Color color) { get()._drawRectangle(pos, size, color); };
+    [[maybe_unused]] static void DrawRectangleRounded(vf2d pos, vf2d size, float radius, Color color) { get()._drawRectangleRounded(pos, size, radius, color); };
+    [[maybe_unused]] static void DrawCircle(vf2d pos, float radius, Color color) { get()._drawCircle(pos, radius, color); };
+    [[maybe_unused]] static void DrawCircleSector(vf2d center, float radius, float startAngle, float endAngle, int segments, Color color) { get()._drawCircleSector(center, radius, startAngle, endAngle, segments, color); };
 
-    static void DrawTexturePart(Texture texture, vf2d pos, vf2d size, Rectangle src)
-    {
-        get()._drawTexturePart(texture, pos, size, src);
-    };
+
+    [[maybe_unused]] static void DrawRectangleFilled(vf2d pos, vf2d size, Color color) { get()._drawRectangle(pos, size, color); };
+    [[maybe_unused]] static void DrawRectangleRoundedFilled(vf2d pos, vf2d size, float radius, Color color) { get()._drawRectangleRounded(pos, size, radius, color); };
+    [[maybe_unused]] static void DrawCircleFilled(vf2d pos, float radius, Color color) { get()._drawCircle(pos, radius, color); };
+    [[maybe_unused]] static void DrawCircleSectorFilled(vf2d center, float radius, float startAngle, float endAngle, int segments, Color color) { get()._drawCircleSector(center, radius, startAngle, endAngle, segments, color); };
+
+
+    [[maybe_unused]] static void DrawTexture(Texture texture, vf2d pos, vf2d size, Color color = WHITE) { get()._drawTexture(texture, pos, size, color); };
+    [[maybe_unused]] static void DrawTexturePart(Texture texture, vf2d pos, vf2d size, Rectangle src, Color color = WHITE) { get()._drawTexturePart(texture, pos, size, src, color); };
+
+    [[maybe_unused]] static void BeginScissorMode(Rectangle area) { get()._beginScissorMode(area); };
+    [[maybe_unused]] static void EndScissorMode() { get()._endScissorMode(); };
+
 
 private:
 
 
 
-
+    void _drawLine(vf2d start, vf2d end, float width, Color color);
     void _drawRectangle(vf2d pos, vf2d size, Color color);
-    void _drawTexture(Texture texture, vf2d pos, vf2d size);
-    void _drawTexturePart(Texture texture, vf2d pos, vf2d size, Rectangle src);
+    void _drawRectangleRounded(vf2d pos, vf2d size, float radius, Color color);
+    void _drawCircle(vf2d pos, float radius, Color color);
+    void _drawCircleSector(vf2d center, float radius, float startAngle, float endAngle, int segments, Color color);
 
+
+
+    void _drawTexture(Texture texture, vf2d pos, vf2d size, Color color = WHITE);
+    void _drawTexturePart(Texture texture, vf2d pos, vf2d size, Rectangle src, Color color = WHITE);
+
+
+    void _beginScissorMode(Rectangle area);
+    void _endScissorMode();
 
     SDL_Renderer* renderer = nullptr;
 
