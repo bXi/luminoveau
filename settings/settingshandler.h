02ac1a257c24b2f9f6e8255c3cc20d598af8b55a
Dennis Meinen <bluepunk@gmail.com>
2023-12-17 20:49:46 +0100

    Cleaned settings handler.

--- settings/settingshandler.h
+++ settings/settingshandler.h
@@ -13,71 +13,26 @@
 class Settings {
 public:
 
-    static void setRes(int width, int height)
-    {
-        get()._setRes(width, height);
-    }
-    static void toggleFullscreen()
-    {
-        get()._toggleFullscreen();
-    }
-    static void toggleVsync()
-    {
-        get()._toggleVsync();
-    }
-    static bool getVsync()
-    {
-        return get()._getVsync();
-    }
-    static void saveSettings()
-    {
-        get()._saveSettings();
-    }
-    static void Init()
-    {
-        get()._init();
-    }
-
-    static std::vector<std::pair<int, int>> resolutions()
-    {
-        return get()._resolutions();
-    }
-
-    static void setMusicVolume(float volume)
-    {
-        get()._setMusicVolume(volume);
-    }
-    static void setSoundVolume(float volume)
-    {
-        get()._setSoundVolume(volume);
-    }
-    static void setMasterVolume(float volume)
-    {
-        get()._setMasterVolume(volume);
-    }
-
-    static float getMusicVolume()
-    {
-        return get()._getMusicVolume();
-    }
-    static float getSoundVolume()
-    {
-        return get()._getSoundVolume();
-    }
-    static float getMasterVolume()
-    {
-        return get()._getMasterVolume();
-    }
-    static int getMonitorRefreshRate()
-    {
-        return get()._getMonitorRefreshRate();
-    }
+    static void setRes(int width, int height) { get()._setRes(width, height); }
+    static void toggleFullscreen() { get()._toggleFullscreen(); }
+    static void toggleVsync() { get()._toggleVsync(); }
+    static bool getVsync() { return get()._getVsync(); }
+    static void saveSettings() { get()._saveSettings(); }
+    static void Init() { get()._init(); }
+    static std::vector<std::pair<int, int>> resolutions() { return get()._resolutions(); }
+    static void setMusicVolume(float volume) { get()._setMusicVolume(volume); }
+    static void setSoundVolume(float volume) { get()._setSoundVolume(volume); }
+    static void setMasterVolume(float volume) { get()._setMasterVolume(volume); }
+    static float getMusicVolume() { return get()._getMusicVolume(); }
+    static float getSoundVolume() { return get()._getSoundVolume(); }
+    static float getMasterVolume() { return get()._getMasterVolume(); }
+    static int getMonitorRefreshRate() { return get()._getMonitorRefreshRate(); }
 
 private:
     bool vsync = true;
     bool fullscreen = false;
-    int  resWidth = 1280;
-    int  resHeight = 720;
+    int resWidth = 1280;
+    int resHeight = 720;
 
     //audio
     float masterVolume = 1.0f;
