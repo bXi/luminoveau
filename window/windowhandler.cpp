d27eb11c344c354a2198e2e35b1a25c24fb21ba8
Dennis Meinen <bluepunk@gmail.com>
2023-12-17 20:23:55 +0100

    Moved function bodies from the header file.

--- window/windowhandler.cpp
+++ window/windowhandler.cpp
@@ -2,6 +2,7 @@
 
 #include <stdexcept>
 #include "audio/audiohandler.h"
+
 void Window::_initWindow(const std::string &title, int width, int height, unsigned int flags) {
 
     // SDL_InitSubSystem is ref-counted
@@ -17,16 +18,16 @@ void Window::_initWindow(const std::string &title, int width, int height, unsign
 
     SDL_SetRenderDrawBlendMode(_getRenderer(), SDL_BLENDMODE_BLEND);
 
-    #ifdef ADD_IMGUI
+#ifdef ADD_IMGUI
     ImGui::CreateContext();
 
     ImGui_ImplSDL3_InitForSDLRenderer(window, GetRenderer());
     ImGui_ImplSDLRenderer3_Init(GetRenderer());
-    #endif
+#endif
 
 }
 
-void Window::_onCloseWindow() {
+void Window::_close() {
     // SDL_QuitSubSystem is ref-counted
     Audio::Close();
     SDL_Quit();
@@ -38,17 +39,8 @@ double Window::_getRunTime() {
     return (double) SDL_GetTicks() * 1000.0;
 }
 
-vi2d Window::_getWindowSize() {
-    int w, h;
-
-    auto *window = get().m_window.get();
-    SDL_GetWindowSize(window, &w, &h);
-
-    return {w, h};
-}
-
 void Window::_toggleFullscreen() {
-    auto *window = get().m_window.get();
+    auto *window = m_window.get();
 
     auto fullscreenFlag = SDL_WINDOW_FULLSCREEN;
     bool isFullscreen = SDL_GetWindowFlags(window) & fullscreenFlag;
@@ -57,26 +49,97 @@ void Window::_toggleFullscreen() {
 }
 
 SDL_Renderer *Window::_getRenderer() {
-    auto *window = get().m_window.get();
+    auto *window = m_window.get();
     return SDL_GetRenderer(window);
 }
 
 int Window::_getFPS(float milliseconds) {
     auto seconds = milliseconds / 1000.f;
 
-    if (get()._fpsAccumulator > seconds) {
-        get()._fpsAccumulator -= seconds;
+    if (_fpsAccumulator > seconds) {
+        _fpsAccumulator -= seconds;
 
-        get()._fps = (int)(1. / get()._lastFrameTime);
+        _fps = (int) (1. / _lastFrameTime);
     }
-    return get()._fps;
+    return _fps;
 }
 
 bool Window::_isFullscreen() {
-    auto *window = get().m_window.get();
+    auto *window = m_window.get();
 
     auto flag = SDL_GetWindowFlags(window);
-	auto is_fullscreen  = flag&SDL_WINDOW_FULLSCREEN;
+    auto is_fullscreen = flag & SDL_WINDOW_FULLSCREEN;
     return is_fullscreen == SDL_WINDOW_FULLSCREEN;
 }
+
+vf2d Window::_getSize() {
+    int w, h;
+
+    SDL_GetWindowSize(m_window.get(), &w, &h);
+
+    return {(float) w, (float) h};
+}
+
+void Window::_handleInput() {
+    Input::Update();
+
+    SDL_Event event;
+
+
+    while (SDL_PollEvent(&event)) {
+#ifdef ADD_IMGUI
+        ImGui_ImplSDL3_ProcessEvent(&event);
+#endif
+
+        switch (event.type) {
+            case SDL_EventType::SDL_EVENT_QUIT:
+                _shouldQuit = true;
+                break;
+        }
+    }
+
+}
+
+
+void Window::_setSize(int width, int height) {
+    SDL_SetWindowSize(m_window.get(), width, height);
+}
+
+
+void Window::_clearBackground(Color color) {
+    SDL_SetRenderDrawColor(GetRenderer(), color.r, color.g, color.b, color.a);
+    SDL_RenderClear(GetRenderer());
+}
+
+void Window::_startFrame() {
+    Lerp::updateLerps();
+
+    Window::HandleInput();
+    SDL_SetRenderDrawColor(GetRenderer(), 0, 0, 0, 255);
+    SDL_RenderClear(GetRenderer());
+#ifdef ADD_IMGUI
+    ImGui_ImplSDLRenderer3_NewFrame();
+    ImGui_ImplSDL3_NewFrame();
+    ImGui::NewFrame();
+#endif
+}
+
+void Window::_endFrame() {
+#ifdef ADD_IMGUI
+    ImGui::Render();
+    ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData());
+#endif
+    SDL_RenderPresent(Window::GetRenderer());
+
+    _frameCount++;
+    _previousTime = _currentTime;
+    _currentTime = std::chrono::high_resolution_clock::now();
+    _lastFrameTime = duration_cast<std::chrono::nanoseconds>(_currentTime - _previousTime).count() / 1000000000.;
+    _fpsAccumulator += _lastFrameTime;
+}
+
+SDL_Window *Window::_getWindow() {
+    return m_window.get();
+}
+
 
--- window/windowhandler.h
+++ window/windowhandler.h
@@ -39,116 +39,59 @@ public:
     {
         get()._initWindow(title, width, height, flags);
     }
+    static void Close() { get()._close(); }
 
-    static SDL_Window* GetWindow() {
-        return get().m_window.get();
-    }
+    static SDL_Window* GetWindow() { return get()._getWindow(); }
+    static SDL_Renderer *GetRenderer() { return get()._getRenderer(); };
 
+    static void SetSize(int width, int height) { get()._setSize(width, height); }
+    static vf2d GetSize() { return get()._getSize(); }
+    static int GetWidth() { return get()._getSize().x; }
+    static int GetHeight() { return get()._getSize().y; }
 
-    static vf2d GetSize() {
-        int w, h;
+    static void StartFrame() { get()._startFrame(); }
+    static void EndFrame() { get()._endFrame(); }
 
-        SDL_GetWindowSize(get().m_window.get(), &w, &h);
+    static void ClearBackground(Color color) { get()._clearBackground(color); }
 
-        return {(float)w, (float)h};
-    }
-
-    static void SetSize(int width, int height) {
-        SDL_SetWindowSize(get().m_window.get(), width, height);
-    }
-
-    static void StartFrame() {
-        Lerp::updateLerps();
-
-        Window::HandleInput();
-        SDL_SetRenderDrawColor(GetRenderer(), 0, 0,0,255);
-        SDL_RenderClear(GetRenderer());
-    #ifdef ADD_IMGUI
-        ImGui_ImplSDLRenderer3_NewFrame();
-        ImGui_ImplSDL3_NewFrame();
-        ImGui::NewFrame();
-    #endif
-    }
-
-    static void ClearBackground(Color color) {
-        SDL_SetRenderDrawColor(GetRenderer(), color.r, color.g,color.b,color.a);
-        SDL_RenderClear(GetRenderer());
-    }
-
-    static void EndFrame() {
-        #ifdef ADD_IMGUI
-        ImGui::Render();
-        ImGui_ImplSDLRenderer3_RenderDrawData(ImGui::GetDrawData());
-        #endif
-        SDL_RenderPresent(Window::GetRenderer());
-
-        get()._frameCount++;
-        get()._previousTime = get()._currentTime;
-        get()._currentTime = std::chrono::high_resolution_clock::now();
-        get()._lastFrameTime = duration_cast<std::chrono::nanoseconds >( get()._currentTime - get()._previousTime ).count() / 1000000000.;
-        get()._fpsAccumulator += get()._lastFrameTime;
-    }
     static void ToggleFullscreen() { get()._toggleFullscreen(); }
     static bool IsFullscreen() { return get()._isFullscreen(); }
     static double GetRunTime() { return duration_cast<std::chrono::milliseconds >( get()._currentTime - get()._startTime ).count() / 1000.; }
-    static int GetWidth() { return get()._getWindowSize().x; }
-    static int GetHeight() { return get()._getWindowSize().y; }
+
     static bool ShouldQuit() { return get()._shouldQuit; }
     static double GetFrameTime() { return get()._lastFrameTime; }
     static int GetFPS(float milliseconds = 400.f) { return get()._getFPS(milliseconds); }
 
-    static void HandleInput() {
-        Input::Update();
-
-        SDL_Event event;
-
-
-        while (SDL_PollEvent(&event)) {
-            #ifdef ADD_IMGUI
-            ImGui_ImplSDL3_ProcessEvent(&event);
-            #endif
-
-            switch (event.type) {
-                case SDL_EventType::SDL_EVENT_QUIT:
-                    get()._shouldQuit = true;
-                    break;
-            }
-        }
-
-    }
-
-    static void Close() {
-        get()._onCloseWindow();
-    }
-
-    static SDL_Renderer *GetRenderer() { return get()._getRenderer(); };
+    static void HandleInput() { get()._handleInput(); }
 
 private:
     std::unique_ptr<SDL_Window, SDL_Window_deleter> m_window;
 
-
-    bool _shouldQuit = false;
     double _getRunTime();
     void _initWindow(const std::string &title, int width = 800, int height = 600, unsigned int flags = 0);
-    void _onCloseWindow();
-    vi2d _getWindowSize();
+    void _close();
     void _toggleFullscreen();
     bool _isFullscreen();
     SDL_Renderer *_getRenderer();
+    vf2d _getSize();
+    void _handleInput();
+    int _getFPS(float milliseconds);
 
+    SDL_Window* _getWindow();
+    void _setSize(int width, int height);
+    void _startFrame();
+    void _clearBackground(Color color);
+    void _endFrame();
+
+    bool _shouldQuit = false;
 
     int _frameCount;
     int _fps;
     double _lastFrameTime;
-
     double _fpsAccumulator;
     std::chrono::high_resolution_clock::time_point _startTime;
-
     std::chrono::high_resolution_clock::time_point _currentTime;
     std::chrono::high_resolution_clock::time_point _previousTime;
-
-    int _getFPS(float milliseconds);
-
 
 public:
     Window(const Window&) = delete;
