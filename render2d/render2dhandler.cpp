f1c7bcecb4bc0679b55e26ca991063f9f79a0076
Dennis Meinen <bluepunk@gmail.com>
2023-12-22 19:21:18 +0100

    Filled in a lot of rendering methods.

--- render2d/render2dhandler.cpp
+++ render2d/render2dhandler.cpp
@@ -24,12 +24,11 @@ void Render2D::_drawRectangleRounded(vf2d pos, vf2d size, float radius, Color co
 
 }
 
-void Render2D::_drawLine(vf2d start, vf2d end, float width, Color color) {
-    SDL_SetRenderDrawColor(renderer, color.r, color.g,color.b,color.a);
-    SDL_RenderLine(renderer, start.x, start.y, end.x, end.y);
+void Render2D::_drawLine(vf2d start, vf2d end, Color color) {
+    lineRGBA(Window::GetRenderer(), start.x, start.y, end.x, end.y, color.r, color.r, color.g, color.a);
 }
 
-void Render2D::_drawCircleSector(vf2d center, float radius, float startAngle, float endAngle, int segments, Color color) {
+void Render2D::_drawArc(vf2d center, float radius, float startAngle, float endAngle, int segments, Color color) {
 
 }
 
@@ -75,7 +74,10 @@ void Render2D::_drawRectangleFilled(vf2d pos, vf2d size, Color color) {
 
 void Render2D::_drawRectangleRoundedFilled(vf2d pos, vf2d size, float radius, Color color) {
 
-    //TODO: Fix this. middle part of the roundedBox isn't drawn properly
+    if (Camera::IsActive()) {
+        pos = Camera::ToScreenSpace(pos);
+        radius *= Camera::GetScale();
+    }
     roundedBoxRGBA(Window::GetRenderer(),
                          pos.x, pos.y,
                          pos.x + size.x, pos.y + size.y,
@@ -87,9 +89,16 @@ void Render2D::_drawRectangleRoundedFilled(vf2d pos, vf2d size, float radius, Co
 
 void Render2D::_drawCircleFilled(vf2d pos, float radius, Color color) {
 
+    if (Camera::IsActive()) {
+        // Convert world space coordinates to screen space
+        pos = Camera::ToScreenSpace(pos);
+
+    }
+
+    filledCircleRGBA(Window::GetRenderer(), pos.x, pos.y, radius, color.r, color.g, color.b, color.a);
 }
 
-void Render2D::_drawCircleSectorFilled(vf2d center, float radius, float startAngle, float endAngle, int segments, Color color) {
+void Render2D::_drawArcFilled(vf2d center, float radius, float startAngle, float endAngle, int segments, Color color) {
 
 }
 
@@ -98,7 +107,6 @@ void Render2D::_beginMode2D() {
     // Clear the screen
     Camera::Activate();
 }
-
 
 void Render2D::_endMode2D() {
     Camera::Deactivate();
@@ -120,6 +128,85 @@ Rectangle Render2D::_doCamera(vf2d pos, vf2d size) {
     }
 
     return dstRect;
+}
+
+void Render2D::_drawThickLine(vf2d start, vf2d end, Color color, float width) {
+
+    if (Camera::IsActive()) {
+        // Convert world space coordinates to screen space
+        start = Camera::ToScreenSpace(start);
+        end = Camera::ToScreenSpace(end);
+
+        width *= Camera::GetScale();
+    }
+
+    thickLineRGBA(
+            Window::GetRenderer(),
+            start.x, start.y,
+            end.x, end.y,
+            (int)width,
+            color.r, color.g, color.b, color.a);
+}
+
+void Render2D::_drawTriangle(vf2d v1, vf2d v2, vf2d v3, Color color) {
+    if (Camera::IsActive()) {
+        // Convert world space coordinates to screen space
+        v1 = Camera::ToScreenSpace(v1);
+        v2 = Camera::ToScreenSpace(v2);
+        v3 = Camera::ToScreenSpace(v3);
+    }
+    _drawLine(v1, v2, color);
+    _drawLine(v2, v3, color);
+    _drawLine(v3, v1, color);
+//TODO: figure out why this doesn't draw
+//    trigonRGBA(Window::GetRenderer(),
+//               v1.x, v1.y,
+//               v2.x, v2.y,
+//               v3.x, v3.y,
+//                color.r, color.g, color.b, color.a);
+}
+
+void Render2D::_drawEllipse(vf2d center, float radiusX, float radiusY, Color color) {
+
+    if (Camera::IsActive()) {
+        // Convert world space coordinates to screen space
+        center = Camera::ToScreenSpace(center);
+    }
+
+
+    ellipseRGBA(Window::GetRenderer(),
+               center.x, center.y,
+               radiusX,
+               radiusY,
+                color.r, color.g, color.b, color.a);
+}
+
+void Render2D::_drawTriangleFilled(vf2d v1, vf2d v2, vf2d v3, Color color) {
+    if (Camera::IsActive()) {
+        // Convert world space coordinates to screen space
+        v1 = Camera::ToScreenSpace(v1);
+        v2 = Camera::ToScreenSpace(v2);
+        v3 = Camera::ToScreenSpace(v3);
+    }
+
+    filledTrigonRGBA(Window::GetRenderer(),
+               v1.x, v1.y,
+               v2.x, v2.y,
+               v3.x, v3.y,
+                color.r, color.g, color.b, color.a);
+}
+
+void Render2D::_drawEllipseFilled(vf2d center, float radiusX, float radiusY, Color color) {
+    if (Camera::IsActive()) {
+        // Convert world space coordinates to screen space
+        center = Camera::ToScreenSpace(center);
+    }
+
+    filledEllipseRGBA(Window::GetRenderer(),
+               center.x, center.y,
+               radiusX,
+               radiusY,
+                color.r, color.g, color.b, color.a);
 }
 
 
--- render2d/render2dhandler.h
+++ render2d/render2dhandler.h
@@ -16,17 +16,23 @@
 
 class Render2D {
 public:
-    [[maybe_unused]] static void DrawLine(vf2d start, vf2d end, float width, Color color) { get()._drawLine(start, end, width, color); };
+    [[maybe_unused]] static void DrawLine(vf2d start, vf2d end, Color color) { get()._drawLine(start, end, color); };
+    [[maybe_unused]] static void DrawThickLine(vf2d start, vf2d end, Color color, float width) { get()._drawThickLine(start, end, color, width); };
 
+
+    [[maybe_unused]] static void DrawTriangle(vf2d v1, vf2d v2, vf2d v3, Color color) { get()._drawTriangle(v1, v2, v3, color); };
     [[maybe_unused]] static void DrawRectangle(vf2d pos, vf2d size, Color color) { get()._drawRectangle(pos, size, color); };
     [[maybe_unused]] static void DrawRectangleRounded(vf2d pos, vf2d size, float radius, Color color) { get()._drawRectangleRounded(pos, size, radius, color); };
     [[maybe_unused]] static void DrawCircle(vf2d pos, float radius, Color color) { get()._drawCircle(pos, radius, color); };
-    [[maybe_unused]] static void DrawCircleSector(vf2d center, float radius, float startAngle, float endAngle, int segments, Color color) { get()._drawCircleSector(center, radius, startAngle, endAngle, segments, color); };
+    [[maybe_unused]] static void DrawEllipse(vf2d center, float radiusX, float radiusY, Color color) { get()._drawEllipse(center, radiusX, radiusY, color); };
+    [[maybe_unused]] static void DrawArc(vf2d center, float radius, float startAngle, float endAngle, int segments, Color color) { get()._drawArc(center, radius, startAngle, endAngle, segments, color); };
+    [[maybe_unused]] static void DrawTriangleFilled(vf2d v1, vf2d v2, vf2d v3, Color color) { get()._drawTriangleFilled(v1, v2, v3, color); };
 
     [[maybe_unused]] static void DrawRectangleFilled(vf2d pos, vf2d size, Color color) { get()._drawRectangleFilled(pos, size, color); };
     [[maybe_unused]] static void DrawRectangleRoundedFilled(vf2d pos, vf2d size, float radius, Color color) { get()._drawRectangleRoundedFilled(pos, size, radius, color); };
     [[maybe_unused]] static void DrawCircleFilled(vf2d pos, float radius, Color color) { get()._drawCircleFilled(pos, radius, color); };
-    [[maybe_unused]] static void DrawCircleSectorFilled(vf2d center, float radius, float startAngle, float endAngle, int segments, Color color) { get()._drawCircleSectorFilled(center, radius, startAngle, endAngle, segments, color); };
+    [[maybe_unused]] static void DrawEllipseFilled(vf2d center, float radiusX, float radiusY, Color color) { get()._drawEllipseFilled(center, radiusX, radiusY, color); };
+    [[maybe_unused]] static void DrawArcFilled(vf2d center, float radius, float startAngle, float endAngle, int segments, Color color) { get()._drawArcFilled(center, radius, startAngle, endAngle, segments, color); };
 
     [[maybe_unused]] static void DrawTexture(Texture texture, vf2d pos, vf2d size, Color color = WHITE) { get()._drawTexture(texture, pos, size, color); };
     [[maybe_unused]] static void DrawTexturePart(Texture texture, vf2d pos, vf2d size, Rectangle src, Color color = WHITE) { get()._drawTexturePart(texture, pos, size, src, color); };
@@ -38,17 +44,22 @@ public:
     [[maybe_unused]] static void EndMode2D() { get()._endMode2D(); };
 
 private:
-    void _drawLine(vf2d start, vf2d end, float width, Color color);
+    void _drawLine(vf2d start, vf2d end, Color color);
+    void _drawThickLine(vf2d start, vf2d end, Color color, float width);
 
+    void _drawTriangle(vf2d v1, vf2d v2, vf2d v3, Color color);
     void _drawRectangle(vf2d pos, vf2d size, Color color);
     void _drawRectangleRounded(vf2d pos, vf2d size, float radius, Color color);
     void _drawCircle(vf2d pos, float radius, Color color);
-    void _drawCircleSector(vf2d center, float radius, float startAngle, float endAngle, int segments, Color color);
+    void _drawEllipse(vf2d center, float radiusX, float radiusY, Color color);
+    void _drawArc(vf2d center, float radius, float startAngle, float endAngle, int segments, Color color);
 
+    void _drawTriangleFilled(vf2d v1, vf2d v2, vf2d v3, Color color);
     void _drawRectangleFilled(vf2d pos, vf2d size, Color color);
     void _drawRectangleRoundedFilled(vf2d pos, vf2d size, float radius, Color color);
     void _drawCircleFilled(vf2d pos, float radius, Color color);
-    void _drawCircleSectorFilled(vf2d center, float radius, float startAngle, float endAngle, int segments, Color color);
+    void _drawEllipseFilled(vf2d center, float radiusX, float radiusY, Color color);
+    void _drawArcFilled(vf2d center, float radius, float startAngle, float endAngle, int segments, Color color);
 
     void _drawTexture(Texture texture, vf2d pos, vf2d size, Color color = WHITE);
     void _drawTexturePart(Texture texture, vf2d pos, vf2d size, Rectangle src, Color color = WHITE);
