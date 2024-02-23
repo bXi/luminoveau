051d67f152777662d98cf602b98c337641d60750
Dennis Meinen <bluepunk@gmail.com>
2024-02-23 22:48:19 +0100

    Changed font methods to accept a font asset instead of a filename/size pair.

--- fonts/fonthandler.cpp
+++ fonts/fonthandler.cpp
@@ -26,14 +26,11 @@ Font Fonts::_getFont(const char* fileName, const int fontSize)
 	}
 }
 
-void Fonts::_drawText(const char *fileName, const int fontSize, vf2d pos, std::string textToDraw, Color color) {
+void Fonts::_drawText(Font font, vf2d pos, std::string textToDraw, Color color) {
 
     if (textToDraw.empty()) return;
 
     SDL_Surface *textSurface;
-
-    Font font = _getFont(fileName, fontSize);
-
 
     textSurface = TTF_RenderUTF8_Blended(font.font, textToDraw.c_str(), color);
 
@@ -48,20 +45,16 @@ void Fonts::_drawText(const char *fileName, const int fontSize, vf2d pos, std::s
 
 }
 
-int Fonts::_measureText(const char *fileName, const int fontSize, std::string textToDraw) {
+int Fonts::_measureText(Font font, std::string textToDraw) {
 
     if (textToDraw.empty()) return 0;
 
     SDL_Surface *textSurface;
 
-    Font font = _getFont(fileName, fontSize);
-
     textSurface = TTF_RenderUTF8_Blended(font.font, textToDraw.c_str(), {255,255,255,255});
     int width = textSurface->w;
 
     SDL_DestroySurface(textSurface);
-
-
 
     return width;
 }
--- fonts/fonthandler.h
+++ fonts/fonthandler.h
@@ -14,15 +14,15 @@
 class Fonts {
 public:
     static Font GetFont(const char* fileName, const int fontSize) { return get()._getFont(fileName, fontSize); }
-    static void DrawText(const char* fileName, const int fontSize, vf2d pos, const char* textToDraw, Color color) { get()._drawText(fileName, fontSize, pos, textToDraw, color); }
-    static int MeasureText(const char *fileName, const int fontSize, std::string text) { return get()._measureText(fileName,fontSize, text); }
+    static void DrawText(Font font, vf2d pos, const char* textToDraw, Color color) { get()._drawText(font, pos, textToDraw, color); }
+    static int MeasureText(Font font, std::string text) { return get()._measureText(font, text); }
 
 private:
     std::unordered_map<std::string, Font> _fonts;
 
     Font _getFont(const char* fileName, int fontSize);
-    void _drawText(const char* fileName, const int fontSize, vf2d pos, std::string textToDraw, Color color);
-    int _measureText(const char *fileName, const int fontSize, std::string text);
+    void _drawText(Font font, vf2d pos, std::string textToDraw, Color color);
+    int _measureText(Font font, std::string text);
 public:
     Fonts(const Fonts&) = delete;
     static Fonts& get() { static Fonts instance; return instance; }
