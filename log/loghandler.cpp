b28cc4d2f9073cb02de78ba429c5732327d06b00
Dennis Meinen <bluepunk@gmail.com>
2024-02-23 22:48:45 +0100

    Updated loghandler to use new font asset parameters.

--- log/loghandler.cpp
+++ log/loghandler.cpp
@@ -4,11 +4,11 @@
 #include "fonts/fonthandler.h"
 
 void Log::_addLine(const char *line1, const char *line2) {
-    //auto font = Fonts::getFont("assets/fonts/APL386.ttf", 20);
+    auto font = Fonts::GetFont("assets/fonts/APL386.ttf", 20);
 
-    headerWidth = std::max(Fonts::MeasureText("assets/fonts/APL386.ttf", 20, line1), headerWidth);
+    headerWidth = std::max(Fonts::MeasureText(font, line1), headerWidth);
 
-    longestLineWidth = std::max(Fonts::MeasureText("assets/fonts/APL386.ttf", 20, line2), longestLineWidth);
+    longestLineWidth = std::max(Fonts::MeasureText(font, line2), longestLineWidth);
 
     lines.emplace_back(line1, line2);
 }
