5135d7b8fdd67a7551fe32dc8c09b93e206f4dd2
Dennis Meinen <bluepunk@gmail.com>
2024-02-26 22:07:05 +0100

    Updated EventBus to allow optional callbacks on user events

--- eventbus/eventbushandler.cpp
+++ eventbus/eventbushandler.cpp
@@ -1,11 +1,75 @@
 #include "eventbushandler.h"
 
-void EventBus::_register(std::string eventName, std::function<void()> callback) {
+
+
+void EventBus::_fire(std::string eventName, std::optional<EventData> eventData) {
+    bool eventFound = false;
+
+    if (eventData.has_value()) {
+        auto datait = _eventsData.find(eventName);
+        if (datait != _eventsData.end()) {
+            for (const auto &callback: datait->second) {
+                callback(eventData.value());
+            }
+            eventFound = true;
+        }
+    } else {
+        auto it = _events.find(eventName);
+        if (it != _events.end()) {
+            for (const auto &callback: it->second) {
+                callback();
+            }
+            eventFound = true;
+        }
+    }
+
+    if (!eventFound) {
+        std::cout << "Event '" << eventName << "' not registered." << std::endl;
+    }
+}
+
+
+
+
+void EventBus::_register(std::string eventName, EventCallback callback) {
     _events[eventName].push_back(callback);
 }
 
-void EventBus::_fire(std::string eventName) {
-    for (auto& callback : _events[eventName]) {
-        callback();
+void EventBus::_register(std::string eventName, EventCallbackData callback) {
+    _eventsData[eventName].push_back(callback);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void EventBus::_register(SystemEvent eventName, EventCallbackData callback) {
+    _systemEvents[eventName].push_back(callback);
+}
+
+void EventBus::_fire(SystemEvent eventName, EventData eventData) {
+    for (auto& callback : _systemEvents[eventName]) {
+        callback(eventData);
     }
 }
+
--- eventbus/eventbushandler.h
+++ eventbus/eventbushandler.h
@@ -1,21 +1,53 @@
 #pragma once
 
+#include <iostream>
 #include <vector>
 #include <unordered_map>
 #include <string>
 #include <functional>
+#include <variant>
+#include <optional>
+
+enum class SystemEvent {
+    GAMEPAD_CONNECTED,
+    GAMEPAD_DISCONNECTED,
+
+    WINDOW_RESIZE,
+    WINDOW_FULLSCREEN,
+};
+
+using EventData = std::unordered_map<std::string, std::variant<int, float, std::string>>;
+
+using EventCallback = std::function<void()>;
+using EventCallbackData = std::function<void(EventData)>;
 
 class EventBus {
 public:
-    static void Register(std::string eventName, std::function<void()> callback) { get()._register(eventName, callback); }
-    static void Fire(std::string eventName) { get()._fire(eventName); }
+    static void Register(std::string eventName, EventCallback callback) { get()._register(eventName, callback); }
+    static void Register(std::string eventName, EventCallbackData callback) { get()._register(eventName, callback); }
 
+    static void Fire(std::string eventName) { get()._fire(eventName, std::nullopt); }
+    static void Fire(std::string eventName, EventData eventData) { get()._fire(eventName, eventData); }
+
+
+
+    static void Register(SystemEvent eventName, EventCallbackData callback) { get()._register(eventName, callback); }
+    static void Fire(SystemEvent eventName, EventData eventData) { get()._fire(eventName, eventData); }
 
 private:
-    void _register(std::string eventName, std::function<void()> callback);
-    void _fire(std::string eventName);
+    void _fire(std::string eventName, std::optional<EventData> eventData);
 
-    std::unordered_map<std::string, std::vector<std::function<void()>>> _events;
+    void _register(std::string eventName, EventCallback callback);
+    void _register(std::string eventName, EventCallbackData callback);
+
+
+
+    void _register(SystemEvent eventName, EventCallbackData callback);
+    void _fire(SystemEvent eventName, EventData eventData);
+
+    std::unordered_map<std::string, std::vector<EventCallback>> _events;
+    std::unordered_map<std::string, std::vector<EventCallbackData>> _eventsData;
+    std::unordered_map<SystemEvent, std::vector<EventCallbackData>> _systemEvents;
 public:
     EventBus(const EventBus&) = delete;
     static EventBus& get() { static EventBus instance; return instance; }
