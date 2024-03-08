6f3799df21f3843b3e3dfb921defa712239452f3
Dennis Meinen <bluepunk@gmail.com>
2024-03-08 22:55:34 +0100

    Added documentation to all user facing methods/classes.

--- window/windowhandler.h
+++ window/windowhandler.h
@@ -34,44 +34,170 @@ union SDL_Event;
 
 using SDL_Window_deleter = void (*)(SDL_Window *);
 
+/**
+ * @brief Provides functionality for managing the application window.
+ */
 class Window {
 public:
-
-
+    /**
+     * @brief Initializes the application window.
+     *
+     * @param title The title of the window.
+     * @param width The width of the window.
+     * @param height The height of the window.
+     * @param scale The scale factor of the window.
+     * @param flags Additional flags for window creation.
+     */
     static void InitWindow(const std::string &title, int width = 800, int height = 600, int scale = 1, unsigned int flags = 0)
     {
         get()._initWindow(title, width, height, scale, flags);
     }
+
+    /**
+     * @brief Closes the application window.
+     */
     static void Close() { get()._close(); }
 
+    /**
+     * @brief Retrieves the SDL window object.
+     *
+     * @return Pointer to the SDL window object.
+     */
     static SDL_Window* GetWindow() { return get()._getWindow(); }
+
+    /**
+     * @brief Retrieves the SDL renderer object.
+     *
+     * @return Pointer to the SDL renderer object.
+     */
     static SDL_Renderer *GetRenderer() { return get()._getRenderer(); };
 
+    /**
+     * @brief Sets the scale factor of the window.
+     *
+     * @param scalefactor The scale factor to set.
+     */
     static void SetScale(int scalefactor) { get()._setScale(scalefactor); }
 
+    /**
+     * @brief Sets the size of the window.
+     *
+     * @param width The width of the window.
+     * @param height The height of the window.
+     */
     static void SetSize(int width, int height) { get()._setSize(width, height); }
+
+    /**
+     * @brief Sets the scaled size of the window.
+     *
+     * @param width The width of the window.
+     * @param height The height of the window.
+     * @param scale The scale factor of the window.
+     */
     static void SetScaledSize(int width, int height, int scale = 0) { get()._setScaledSize(width, height, scale); };
+
+    /**
+     * @brief Retrieves the size of the window.
+     *
+     * @param getRealSize Flag indicating whether to retrieve the real size (not scaled).
+     * @return The size of the window.
+     */
     static vf2d GetSize(bool getRealSize = false) { return get()._getSize(getRealSize); }
+
+    /**
+     * @brief Retrieves the width of the window.
+     *
+     * @param getRealSize Flag indicating whether to retrieve the real size (not scaled).
+     * @return The width of the window.
+     */
     static int GetWidth(bool getRealSize = false) { return get()._getSize(getRealSize).x; }
+
+    /**
+     * @brief Retrieves the height of the window.
+     *
+     * @param getRealSize Flag indicating whether to retrieve the real size (not scaled).
+     * @return The height of the window.
+     */
     static int GetHeight(bool getRealSize = false) { return get()._getSize(getRealSize).y; }
 
+    /**
+     * @brief Starts a new frame for rendering.
+     */
     static void StartFrame() { get()._startFrame(); }
+
+    /**
+     * @brief Ends the current frame.
+     */
     static void EndFrame() { get()._endFrame(); }
+
+    /**
+     * @brief Sets the render target to the specified texture.
+     *
+     * @param target The texture to set as the render target.
+     */
     static void SetRenderTarget(Texture target) { get()._setRenderTarget(target); }
+
+    /**
+     * @brief Resets the render target to the window.
+     */
     static void ResetRenderTarget() { get()._resetRenderTarget(); }
 
-
+    /**
+     * @brief Clears the background of the window with the specified color.
+     *
+     * @param color The color to clear the background with.
+     */
     static void ClearBackground(Color color) { get()._clearBackground(color); }
 
+    /**
+     * @brief Toggles the fullscreen mode of the window.
+     */
     static void ToggleFullscreen() { get()._toggleFullscreen(); }
+
+    /**
+     * @brief Checks if the window is currently in fullscreen mode.
+     *
+     * @return True if the window is in fullscreen mode, false otherwise.
+     */
     static bool IsFullscreen() { return get()._isFullscreen(); }
+
+    /**
+     * @brief Gets the total runtime of the application.
+     *
+     * @return The total runtime of the application in seconds.
+     */
     static double GetRunTime() { return std::chrono::duration_cast<std::chrono::milliseconds >( get()._currentTime - get()._startTime ).count() / 1000.; }
 
+    /**
+     * @brief Checks if the application should quit.
+     *
+     * @return True if the application should quit, false otherwise.
+     */
     static bool ShouldQuit() { return get()._shouldQuit; }
+
+    /**
+     * @brief Gets the time taken to render the last frame.
+     *
+     * @return The time taken to render the last frame in seconds.
+     */
     static double GetFrameTime() { return get()._lastFrameTime; }
+
+    /**
+     * @brief Gets the frames per second (FPS) of the application.
+     *
+     * @param milliseconds The time interval to calculate FPS over.
+     * @return The frames per second (FPS) of the application.
+     */
     static int GetFPS(float milliseconds = 400.f) { return get()._getFPS(milliseconds); }
 
+    /**
+     * @brief Handles input events.
+     */
     static void HandleInput() { get()._handleInput(); }
+
+    /**
+     * @brief Toggles the debug menu.
+     */
     static void ToggleDebugMenu() { get()._toggleDebugMenu(); }
 
 private:
