26f1747fec5baf8d5c45d447679fece4020ddd4b
Dennis Meinen <bluepunk@gmail.com>
2024-03-08 23:02:06 +0100

    Added documentation to all user facing methods/classes.

--- utils/colors.h
+++ utils/colors.h
@@ -80,7 +80,7 @@ struct Color {
      * @return The alpha component (0.0 - 1.0).
      */
     float getAFloat() const { return static_cast<float>(a) / 255.0f; }
-};
+
 #if __has_include("SDL3/SDL.h")
 
     operator SDL_Color() { return SDL_Color({ (Uint8)r, (Uint8)g, (Uint8)b, (Uint8)a }); }
