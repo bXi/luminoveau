8ed6164fce6d245ceeb18acdb3f399a28d185bf7
Dennis Meinen <bluepunk@gmail.com>
2024-03-20 22:55:27 +0100

    Added overall header file for ease of use.

---
+++ luminoveau.h
@@ -0,0 +1,26 @@
+#pragma once
+
+//Asset types
+#include <assettypes/font.h>
+#include <assettypes/music.h>
+#include <assettypes/sound.h>
+#include <assettypes/texture.h>
+
+//Engine components
+#include <audio/audiohandler.h>
+#include <eventbus/eventbushandler.h>
+#include <input/inputhandler.h>
+#include <render2d/render2dhandler.h>
+#include <text/texthandler.h>
+#include <texture/texturehandler.h>
+#include <window/windowhandler.h>
+
+//Utilities
+#include <utils/camera.h>
+#include <utils/colors.h>
+#include <utils/constants.h>
+#include <utils/helpers.h>
+#include <utils/lerp.h>
+#include <utils/rectangles.h>
+#include <utils/vectors.h>
+
