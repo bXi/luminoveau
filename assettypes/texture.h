687784a691beba599235ed3637d743e0b63f7171
Dennis Meinen <bluepunk@gmail.com>
2024-07-23 22:44:41 +0200

    Added id on Texture asset to easily access the internal ID of the texture.

--- assettypes/texture.h
+++ assettypes/texture.h
@@ -8,9 +8,12 @@
  * @brief Represents a texture asset for rendering images using SDL.
  */
 struct TextureAsset {
+    int32_t id;
     int width = -1; /**< Width of the texture. */
     int height = -1; /**< Height of the texture. */
     std::string filename; /**< Filename of the texture image file. */
+
+
 
     SDL_Surface *surface = nullptr; /**< Pointer to the SDL surface representing the texture. */
     SDL_Texture *texture = nullptr; /**< Pointer to the SDL texture. */
