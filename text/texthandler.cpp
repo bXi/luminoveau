faea068246a116f0e717b45fb970427eca39bc6f
Dennis Meinen <bluepunk@gmail.com>
2025-03-16 23:14:01 +0100

    Added textToDraw as "filename" to texture so the renderable copy is pleased.

--- text/texthandler.cpp
+++ text/texthandler.cpp
@@ -19,6 +19,7 @@ void Text::_drawText(Font font, const vf2d &pos, const std::string &textToDraw, 
 
     do {
         TextureAsset tex = {
+            .filename = textToDraw,
             .gpuTexture = sequence->atlas_texture,
             .gpuSampler = Renderer::GetSampler(AssetHandler::GetDefaultTextureScaleMode()),
         };
