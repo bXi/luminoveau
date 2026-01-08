25fc4036ab084abdc0104988d43f655981d5273c
Dennis Meinen <bluepunk@gmail.com>
2026-01-08 13:49:28 +0100

    Updated ImGUI version to latest official release.

--- cmake/ImGuiIntegration.cmake
+++ cmake/ImGuiIntegration.cmake
@@ -9,28 +9,14 @@ endif()
 
 # Checking if ImGui integration is enabled
 if(LUMINOVEAU_BUILD_IMGUI)
-    # Option to use the official ImGui repository (for future use when bugs are fixed)
-    option(LUMINOVEAU_USE_OFFICIAL_IMGUI "Use official ImGui repository instead of custom fork" OFF)
 
-    if(LUMINOVEAU_USE_OFFICIAL_IMGUI)
-        # Fetching official ImGui repository
-        CPMAddPackage(
-            NAME Imgui
-            GITHUB_REPOSITORY ocornut/imgui
-            GIT_TAG 8cc6eee
-            OPTIONS
-                "IMGUI_BUILD_SDL3_BACKEND OFF"
-        )
-        message(STATUS "Using official ImGui repository (ocornut/imgui)")
-    else()
-        # Fetching custom ImGui fork with SDL-GPU support
-        CPMAddPackage(
-            NAME Imgui
-            GITHUB_REPOSITORY bXi/imgui-sdlgpu
-            GIT_TAG d5edda1
-        )
-        message(STATUS "Using custom ImGui fork (bXi/imgui-sdlgpu)")
-    endif()
+    CPMAddPackage(
+        NAME Imgui
+        GITHUB_REPOSITORY ocornut/imgui
+        GIT_TAG 7b3ad4a
+        OPTIONS
+            "IMGUI_BUILD_SDL3_BACKEND OFF"
+    )
 
     # Verifying that ImGui was successfully added
     if(Imgui_ADDED)
@@ -58,13 +44,11 @@ if(LUMINOVEAU_BUILD_IMGUI)
                 "${Imgui_SOURCE_DIR}/backends/imgui_impl_win32.cpp"
                 "${Imgui_SOURCE_DIR}/backends/imgui_impl_sdlgpu3.cpp"
                 "${Imgui_SOURCE_DIR}/backends/imgui_impl_sdl3.cpp"
-                "${Imgui_SOURCE_DIR}/backends/imgui_impl_sdlgpu3_shadercross.h"
             )
         else()
             target_sources(luminoveau PRIVATE
                 "${Imgui_SOURCE_DIR}/backends/imgui_impl_sdlgpu3.cpp"
                 "${Imgui_SOURCE_DIR}/backends/imgui_impl_sdl3.cpp"
-                "${Imgui_SOURCE_DIR}/backends/imgui_impl_sdlgpu3_shadercross.h"
             )
         endif()
 
